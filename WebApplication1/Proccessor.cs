9bf28cd3982d2fede09072d9b8484ef070d99331
Арина Петрунина <petrunina.arina.12@gmail.com>
2021-11-30 19:31:26 +0300

    Добавьте файлы проекта.

---
+++ WebApplication1/Proccessor.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1
+{
+    public class Proccessor
+    {
+        public Proccessor(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddRazorPages();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+
+            // если приложение в процессе разработки
+            if (env.IsDevelopment())
+            {
+                // то выводим информацию об ошибке, при наличии ошибки
+                app.UseDeveloperExceptionPage();
+            }
+            // добавляем возможности маршрутизации
+            app.UseRouting();
+
+            // устанавливаем адреса, которые будут обрабатываться
+            app.UseEndpoints(endpoints =>
+            {
+                // обработка запроса - получаем констекст запроса в виде объекта context
+                endpoints.MapGet("/", async context =>
+                {
+                    // отправка ответа в виде строки "Hello World!"
+                    await context.Response.WriteAsync("Hello World!");
+                });
+            });
+        }
+    }
+}
---
+++ WebApplication1/Startup.cs
@@ -0,0 +1,42 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1
+{
+    public class Startup
+    {
+        IWebHostEnvironment _env;
+        public Startup(IWebHostEnvironment env)
+        {
+            _env = env;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddRazorPages();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            int x = 2;
+            app.Run(async (context) =>
+            {
+                x = x * 2;  //  2 * 2 = 4
+                await context.Response.WriteAsync($"Result: {x}");
+            });
+        }
+    }
+}
